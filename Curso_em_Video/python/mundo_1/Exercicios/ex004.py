9f5d31b98e3291bb3bbdfa757210948a34d9a138
Nicolas <unipnicolas@gmail.com>
2024-09-02 01:33:17 -0300

    Python

--- Curso_em_Video/python/mundo_1/Exercicios/ex004.py
+++ Curso_em_Video/python/mundo_1/Exercicios/ex004.py
@@ -1,2 +1,6 @@
 
 
+a = input('Digite algo')
+print('o tipodo desse valor pe', type)
+print ('Só tem espaços ?', a.isspace())
+#para exibir todos os outras possibilidades para ( a ) so inserrir o resto dos .is
