8db543bc744ca9aafcbac41c133ed6b2be979879
Nicolas <unipnicolas@gmail.com>
2024-09-04 12:00:33 -0300

    Python
    Finalizei Tratando Dados aprendi sobre operadores logicos.

---
+++ Curso_em_Video/python/mundo_1/Exercicios/ex014.py
@@ -0,0 +1,7 @@
+
+
+
+
+c = int (input('Digite a temperatuda: '))
+f = 9 * c / 5 + 32
+print('A temperatura de {}°C coresponde a {}°F!'.format(c, f))
---
+++ Curso_em_Video/python/mundo_1/Exercicios/ex015.py
@@ -0,0 +1,15 @@
+
+
+
+
+print('Aluguem de carros!')
+dias    = int (input('Quantos dias vc alugou: '))
+km      = int (input('Digite quanto Km vc andou: '))
+#dias = R$ 60
+#km   = R$ 0.15
+pago = (dias * 60) + (km * 0.15)
+print ('total a pagar é de R${:.2f}'.format(pago))
+
+
+
+
