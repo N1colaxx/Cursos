bb67adbaafd9b411a3be6e8ff82cd99c051f2f75
Nicolas <unipnicolas@gmail.com>
2024-09-06 11:34:51 -0300

    Python
    sei q o ex019 ta errado, ja vou corrigi-lo to doente e com dor de cabeça ;/

---
+++ Curso_em_Video/python/mundo_1/AULAS/aula08a.py
@@ -0,0 +1,75 @@
+
+# aula sobre o IMPORT, FROM, Random
+
+
+
+
+
+
+# import math
+# num = int (input('Digite um N°: '))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+
+
+# print('--------------------------------------------')
+#
+#
+# from math import sqrt
+#
+# num = int (input('\n  Digite um N°: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+#
+#
+# print('--------------------------------------------')
+#
+#
+# from math import sqrt, floor
+#
+# num = int (input('Digite um N°: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
+#
+
+
+#======================================================================================================================
+
+
+import random
+# from random import randint
+
+num = random.randint(1, 10)
+print(num)
+
+#--------------------------------------------------
+
+#import random
+from random import randint
+
+num = randint(1, 10)
+print(num)
+
+
+#======================================================================================================================
+
+# import emoji
+# print(emoji.emojize("Ola Mundi :bow_and_arrow:"))
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Curso_em_Video/python/mundo_1/Exercicios/ex016.py
@@ -0,0 +1,9 @@
+
+
+
+from math import ceil
+from reprlib import aRepr
+
+num = float (input('Digite um numero com virgula: '))
+arredonda = ceil(num)
+print("O numero {} tem a parte inteira {}".format(num, arredonda))
