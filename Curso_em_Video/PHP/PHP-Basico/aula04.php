8d24e0d17d9415502e06408a5f39c6ef66b72621
Nicolas <unipnicolas@gmail.com>
2024-11-23 00:42:45 -0300

    Start in PHP
    comecei em PHP bem básico ja ja eu to em básico nilvel 2 kkk

---
+++ Curso_em_Video/PHP/PHP-Basico/aula04.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Div Expansível com PHP</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="centralizada">
+
+        <?php
+            echo "<br>Hello World, starting in PHP<br><br>"
+        ?>
+
+        <?php 
+            $idade = 18;
+            $nome = "Nicolas";
+            
+            echo $nome. " tem ".  $idade. " anos!";
+
+            echo "<br> <br> ";
+            echo "$nome tem $idade anos !!";
+            
+            echo "<br> <br> ";
+            echo '$nome tem $idade anos !!';
+
+            
+        ?>
+
+
+
+
+    </div>
+</body>
+</html>
